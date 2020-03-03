93b709da907d7c6a2326bba580efbe1af9c9fca1
LeticiaMBoldo <60079653+LeticiaMBoldo@users.noreply.github.com>
2020-03-02 21:43:45 -0300

    Initial commit

---
+++ Models/Genero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteFilmes.Models
+{
+    [Table("Genero")]
+    public class Genero
+    {
+        [Key]
+        public int idGenero { get; set; }
+
+        [Display(Name = "Nome do Gênero")]
+        [StringLength(50, ErrorMessage = "O NOME DO GENERO DEVE TER NO MAXIMO 50 CARACTERES")]
+        [Required(ErrorMessage = "O NOME DO GÊNERO É OBRIGATÓRIO")]
+        public string NomeGenero { get; set; }
+
+    }
+}
